7687238c205fb862c867ba5ff83348e5aaa6ee6c
chinnonae <chinnonae@gmail.com>
2018-03-08 15:12:31 +0700

    v1.0

--- script.js
+++ script.js
@@ -1,41 +1,88 @@
 let question = []
 let game = {}
+const TIME = 10000
+const SECOND = 1000
+const DECAY = 100
+const EXTRA_TIME = 2000
+let ticker = null
+let timer = TIME + EXTRA_TIME
 
 $.get('questions.json', function (data) {
   question = data
+  console.log(question)
 })
+
+function startTicker() {
+  timer = TIME + EXTRA_TIME
+
+  ticker = setInterval(function () {
+    if (timer <= 0) {
+      timeOut()
+    }
+    timer -= DECAY
+    progress = timer / TIME * 100
+    progressPercent = timer > TIME ? 100 : progress
+    $('.progress-bar-value').css("width", progressPercent + '%')  
+  }, DECAY)
+}
+
+function timeOut() {
+  nextQuestion()
+}
 
 function startGame (numberOfQuestion) {
   game = new Game(numberOfQuestion)
   nextQuestion()
   $('div.page#start').toggleClass('active')
   $('div.page#game').toggleClass('active')
+  $('.score-value').html('0')
 }
 
 function answer (choice) {
+  clearInterval(ticker)
+
   let correct = game.answer(choice)
 
+  let choiceNode = $('#choice\\:' + choice)
+  let background = '#F44336'
   if (correct) {
-    alert('Correct')
+    let score = timer > TIME ? 10 : Math.ceil(timer / SECOND)
+    game.score += score
+    $('.score-value').html(game.score)
+
+    background = '#C6FF00'
   } else {
-    alert('Incorrect')
+    background = '#F44336'
   }
 
-  $('.score').html(game.score)
-  nextQuestion()
+  choiceNode.animate(
+    {
+    'background-color': background
+    }, 
+    1000,
+    'easeInBounce',
+    function() {
+      setTimeout(function () {
+        choiceNode.css('background-color', '')
+        nextQuestion()
+      }, 2000) 
+    }
+  )
 }
 
 function nextQuestion () {
-  question = game.nextQuestion()
+  clearInterval(ticker)
+  let question = game.nextQuestion()
 
   if (!question) {
     endGame()
   } else {
-    $('#question').html(game.count + '. ' + question.question)
-    $('#choice:1').html(question.answers[0])
-    $('#choice:2').html(question.answers[1])
-    $('#choice:3').html(question.answers[2])
-    $('#choice:4').html(question.answers[3])
+    $('#question').html(question.question)
+    $('#choice\\:0').html(question.answers[0])
+    $('#choice\\:1').html(question.answers[1])
+    $('#choice\\:2').html(question.answers[2])
+    $('#choice\\:3').html(question.answers[3])
+    startTicker()
   }
 }
 
@@ -55,7 +102,7 @@ function randomInt (max) {
 
 function Game (numQuestion) {
   this.numQuestion = numQuestion
-  this.question = Array.from(question)
+  this.question = question.slice()
   this.count = 0
   this.score = 0
 }
@@ -76,11 +123,5 @@ Game.prototype.nextQuestion = function () {
 Game.prototype.answer = function (choice) {
   let correct = this.currentQuestion.correctChoice == choice
 
-  if (correct) {
-    this.score++
-  }
-
   return correct
 }
-
-
